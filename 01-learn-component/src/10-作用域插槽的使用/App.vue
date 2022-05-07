b6c15a9ebfd303a063992bf62af3ff48c4c0a743
xionglp <785874752@qq.com>
2022-05-07 18:06:56 +0800

    作用域插槽用法

--- 01-learn-component/src/10-作用域插槽的使用/App.vue
+++ 01-learn-component/src/10-作用域插槽的使用/App.vue
@@ -1,12 +1,26 @@
 <template>
   <div>
-
+    <show-names :names="names">
+      <template v-slot:default="slotProps">
+        <button>{{slotProps.item}} - {{slotProps.index}}</button>
+      </template>
+    </show-names>
   </div>
 </template>
 
 <script>
+
+  import ShowNames from "./ShowNames.vue"
+
   export default {
-    
+    components: {
+      ShowNames
+    },
+    data() {
+      return {
+        names: ["xionglp", "kobe", "james", "coderwhy"]
+      }
+    }
   }
 </script>
 
