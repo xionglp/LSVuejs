cddf666ecc9266b6273a4744c40e63171eeb66d9
xionglp <785874752@qq.com>
2022-05-09 22:13:09 +0800

    03-vue-composition-api
    初始化项目代码

---
+++ 03-learn-composition-api/src/01-mixin-extends/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>hello world</h2>
+  </div>
+</template>
+
+<script>
+
+  // mixin 混入
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ 03-learn-composition-api/src/main.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import App from './01-mixin-extends/App.vue'
+
+createApp(App).mount('#app')
