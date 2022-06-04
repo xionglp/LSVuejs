7645c652eb5bead6cfbcc2a776ab7f0f95d2832c
xionglp <785874752@qq.com>
2022-06-04 14:03:21 +0800

    vuex基本使用

--- 07-learn-vuex/src/App.vue
+++ 07-learn-vuex/src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h2>App: {{$store.state.counter}}</h2>
+    <button @click="increment">+1</button>
+    <button @click="decrement">+1</button>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    increment() {
+      this.$store.commit("increment")
+    },
+    decrement() {
+      this.$store.commit("decrement")
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
