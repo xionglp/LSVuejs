7df6437794ab7e1848d0b3de95b08a32a3860ee8
xionglp <785874752@qq.com>
2022-06-04 12:07:23 +0800

    导航守卫

--- 06-learn-vuerouter/src/router/index.js
+++ 06-learn-vuerouter/src/router/index.js
@@ -43,6 +43,10 @@ const routes = [
     component: () => import("../pages/User.vue"),
   },
   {
+    path: "/login",
+    component: () => import("../pages/Login.vue"),
+  },
+  {
     path: "/:pathMatch(.*)",
     component: () => import("../pages/NotFound.vue"),
   },
@@ -64,6 +68,22 @@ router.addRoute(categoryRoute);
 router.addRoute("home", {
   path: "moment",
   component: () => import("../pages/HomeMoment.vue"),
+})
+
+// 导航守卫
+// 不同返回值做的操作：
+// 1、false： 不进行导航
+// 2、undefined或者不写返回值则进行默认导航
+// 3、字符串： 路径，跳转到对应的路径中
+// 4、对象 类似于router.push({path: "/login", query: {name: xionglp...}})
+router.beforeEach((to) => {
+  console.log(to);
+  if (to.path !== "/login") {
+    const token = window.localStorage.getItem("token");
+    if (!token) {
+      return "/login"
+    }
+  }
 })
 
 export default router;
