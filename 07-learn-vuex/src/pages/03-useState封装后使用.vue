0b5d961b800ff2e22f94c8aa19e818be12e7d4d1
xionglp <785874752@qq.com>
2022-06-06 14:33:12 +0800

    vuex getters基本使用

---
+++ 07-learn-vuex/src/pages/03-useState封装后使用.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h2>Home counter: {{ $store.state.counter }}</h2>
+    <hr>
+    <h2>Home counter: {{ counter }}</h2>
+    <h2>Home name: {{ name }}</h2>
+    <h2>Home age: {{ age }}</h2>
+    <h2>Home height: {{sHeight}}</h2>
+    <hr>
+  </div>
+</template>
+
+<script>
+  import { useState } from "../hooks/useState.js"
+
+  export default {
+    setup() {
+      const storeState = useState(["counter", "name", "age"])
+      const storeState2 = useState({
+        sHeight: state => state.height
+      })
+
+      return {
+        ...storeState,
+        ...storeState2
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- 07-learn-vuex/src/pages/Home.vue
+++ 07-learn-vuex/src/pages/Home.vue
@@ -1,30 +1,15 @@
 <template>
   <div>
-    <h2>Home: {{ $store.state.counter }}</h2>
-    <h2>Home: {{ $store.state.name }}</h2>
-    <!-- <h2>Home Age: {{ age }}</h2> -->
-    <h2>Home Height: {{ customHeight }}</h2>
+    <hr>
+      <h2>总价: {{$store.getters.totalPrice}}</h2>
+      <h2>总价: {{$store.getters.totalPriceCountGreaterN(1)}}</h2>
+    <hr>
   </div>
 </template>
 
 <script>
-  import { mapState } from "vuex";
-
   export default {
-    computed: {
-      fullName() {
-        return "Kobe Bryant"
-      },
-      // 其他的计算属性，从state获取
-      // ...mapState(["counter", "age", "name", "height"])
-      ...mapState({
-        customName: state => state.name,
-        customHeight: state => state.height,
-      })
-    },
-    methods: {
 
-    }
   }
 </script>
 
